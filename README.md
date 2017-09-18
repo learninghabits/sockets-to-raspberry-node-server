36a302e06784bb44536f68ffbee05b10a645f79c
TJ <tshepo.ntlhokoa@gmail.com>
2017-09-18 20:05:37 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# sockets-to-raspberry-node-server
+
+1. Installing GIT on your machine (Linux)
+
+sudo apt-get update
+sudo apt-get install git
+
+2. 
---
+++ server.js
@@ -0,0 +1,44 @@
+﻿var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', function (socket) {
+	socket.on('disconnect', function () {
+		console.log('client disconnected with ID : ' + socket.id);
+	});
+	console.log('client connected with ID : ' + socket.id);
+});
+
+app.get('/device/on', function (request, response) {
+	io.emit('SwitchOn');
+	response.status(200)
+		.send('OK');
+});
+
+app.get('/device/off', function (request, response) {
+	io.emit('SwitchOff');
+	response.status(200)
+		.send('OK');
+});
+
+app.get('/device/togglestate/:seconds', function (request, response) {
+	var seconds = request.query.Seconds || request.query.seconds;
+	io.emit('ToggleState', { seconds: seconds });
+	response.status(200)
+		.send('OK');
+});
+
+app.get('*', function (req, res) {
+	res.send(
+		`This sockets server is ready for get requests:
+			/device/on
+			/device/off
+			/togglestate/:seconds`);
+});
+
+var port = process.argv.slice(2)[0] || (process.env.PORT || 80);
+http.listen(port, function () {
+	console.log("SERVER IS LISTENING ON PORT: " + port);
+	console.log("CTRL+C TO STOP ");
+});
